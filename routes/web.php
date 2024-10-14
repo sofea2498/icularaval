6efc2d68e1427a282adabf098c4ef75f74ba9a24
sofea2498 <sofea.sufian@icu.gov.my>
2024-10-14 15:41:19 +0800

    init commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/auth/signin', function () {
+    return view('auth.signin');
+});
+
