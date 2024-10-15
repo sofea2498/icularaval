877f181335b2f10cfa9631e7a802bf9ae63ab128
sofea2498 <sofea.sufian@icu.gov.my>
2024-10-15 10:32:02 +0800

    1. Add 'nama' variables
    2. Change title of the file and add a header
    
    3. Add named routes, route parameter, route group and combination route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
 
-    <h1>Hello ICU Team</h1>
+    <h1>Hello , {{ $name ?? "team"}}</h1>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,66 @@ Route::get('/auth/signin', function () {
     return view('auth.signin');
 });
 
+//Named Route
+Route::get('/user/profile', function () {
+    return 'Pengguna Profile Baru';
+})->name('user.profile');
+
+//Route Parameter
+Route::get('/user/{name}', function ($name,) {
+    return 'User '.$name;
+});
+
+//Roure Parameter
+Route::get('/user/{name}/{age}', function ($name,$age) {
+    return 'User '.$name . 'age is ' .$age;
+});
+
+// //other example route parameter
+// Route::get('/user/{name}', function ($name,) {
+//     return 'User '.$name;
+// });
+
+//Named Route -> alias of a route userprofile = /pegguna/profile
+Route::get('/people/profile', function () {
+    return 'Pengguna Profiles';
+})->name('user.profile');
+
+//Redirect route to named route 
+Route::get('/redirect-to-profile', function () {
+    return redirect()->route('user.profile');
+});
+
+//Route Group -> to group the function folder, maksudnya macam tak payah nak tulis banyak  kali dekat dalam coding kalau dalam browser yg sama
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+    
+});
+
+
+//Combination of all above above (route, named route, route param, route group, route prefix)
+Route::name('job')->prefix('job')->group(function () {
+
+    Route::get('/home', function ($name) {
+        return 'Job name: '.$name;
+    })->name('.home');
+
+    Route::get('/details', function () {
+        return 'Job details are following';
+    })->name('.description');
+    
+});
+
+//to display some values using parameter
+Route::get('/home/{name}', function ($name) {
+    return view('home', ['name' => $name ]);
+});
+
+
